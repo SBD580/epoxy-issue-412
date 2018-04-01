5d6de32be24dc25bfbf0551f40eac2cdbf944208
Shay Ben David <shay@prospera.ag>
2018-04-01 23:58:18 +0300

    ...

---
+++ app/src/main/java/com/sbd580/eproxyissue412/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sbd580.eproxyissue412;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/sbd580/eproxyissue412/package-info.java
@@ -0,0 +1,8 @@
+@EpoxyDataBindingLayouts({R.layout.activity_main})
+@PackageModelViewConfig(rClass = R.class)
+package com.sbd580.eproxyissue412;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
+import com.airbnb.epoxy.PackageEpoxyConfig;
+import com.airbnb.epoxy.PackageModelViewConfig;
+import com.sbd580.eproxyissue412.R;
